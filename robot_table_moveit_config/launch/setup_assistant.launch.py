0cd01b4ead1a69e57f4d9b97370480a6648cea70
TBe <tamara.bergerhoff@mainfranken-racing.de>
2026-06-16 16:13:29 +0200

    📖 DOC: env variables launch description

--- robot_table_moveit_config/launch/setup_assistant.launch.py
+++ robot_table_moveit_config/launch/setup_assistant.launch.py
@@ -1,6 +1,11 @@
 from moveit_configs_utils import MoveItConfigsBuilder
 from moveit_configs_utils.launches import generate_setup_assistant_launch
 
+# setup assistant (wichtige umgebungsvariablen, damit es funktioniert): 
+# export DISPLAY=:0
+# export QT_QPA_PLATFORM=xcb
+# export LIBGL_ALWAYS_SOFTWARE=1
+# export WAYLAND_DISPLAY=wayland-0
 
 def generate_launch_description():
     moveit_config = MoveItConfigsBuilder("robot_table", package_name="robot_table_moveit_config").to_moveit_configs()
